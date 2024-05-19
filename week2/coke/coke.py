212db0c8aded87cec9c0af88b810bfae9544fa27
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-19 16:57:07 -0500

    Update coke.py

--- week2/coke/coke.py
+++ week2/coke/coke.py
@@ -1,1 +1,13 @@
+price = 50
+inserted = 0
+
+while inserted < 50:
+    print(f"Amount Due: {price - inserted}")
+    insert = int(input("Insert Coin: "))
+    if insert == 5 or insert == 10 or insert == 25:
+        inserted += insert
+    else:
+        continue
+
+print(f"Change Owed: {abs(price - inserted)}")
 
