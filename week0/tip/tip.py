87e62a8e5a9f9e40e2ef23b9b8e84e5e3c519e9b
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-19 13:55:01 -0500

    Update tip.py

--- week0/tip/tip.py
+++ week0/tip/tip.py
@@ -1,1 +1,18 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
 
+
+def dollars_to_float(d):
+    new_d = d.replace("$", "")
+    return float(new_d)
+
+def percent_to_float(p):
+    new_p = p.replace("%", "")
+    p_dec = float(new_p) / 100
+    return p_dec
+
+if __name__ == "__main__":
+    main()
