7e6f6440026f666fdbaacdfe95446770e6f911a3
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-19 16:34:26 -0500

    Update camel.py

--- week2/camel/camel.py
+++ week2/camel/camel.py
@@ -1,1 +1,17 @@
+def main():
+    camel = input("camelCase: ")
+    print("snake_case: " + snake_case(camel))
+
+def snake_case(s):
+    snake = []
+    for c in s:
+        if c.isupper():
+            snake.append("_" + c)
+        else:
+            snake.append(c)
+    return ''.join(snake).lower()
+
+
+if __name__ == "__main__":
+    main()
 
