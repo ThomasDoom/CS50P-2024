c807b2f902fcc9ded50c10c9b7b07d80bc0216ce
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-19 16:34:51 -0500

    Create camel_revision.py

---
+++ week2/camel/camel_revision.py
@@ -0,0 +1,31 @@
+def main():
+    camel = input("camelCase: ")
+    print("snake_case:", convert_snake_case(camel))
+
+
+def convert_snake_case(string):
+    output = ""
+    for c in string:
+        if c.isupper():
+            output += '_' + c.lower()
+        else:
+            output += c
+    return output
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+# camel = input("camelCase: ")
+# output = ""
+
+# for c in camel:
+#     if c.isupper():
+#         output += '_' + c.lower()
+#     else:
+#         output += c
+
+# print("snake_case:", output)
