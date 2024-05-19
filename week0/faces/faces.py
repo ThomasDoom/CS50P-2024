17d6bef218a905a10da283cb8ac8360a1de6c24d
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-19 13:52:20 -0500

    Update faces.py

--- week0/faces/faces.py
+++ week0/faces/faces.py
@@ -1,1 +1,11 @@
+def main():
+    string = input()
+    print(convert(string))
 
+def convert(txt):
+    txt = txt.replace(":(", '🙁')
+    txt = txt.replace(":)", '🙂')
+    return txt
+
+if __name__ == "__main__":
+    main()
