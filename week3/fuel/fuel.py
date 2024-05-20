301529a255fa62a655fe212ac17577bd400e829e
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-20 17:05:18 -0500

    Update fuel.py

--- week3/fuel/fuel.py
+++ week3/fuel/fuel.py
@@ -1,1 +1,27 @@
+def main():
+    print(get_fraction("Fraction: "))
+
+
+def get_fraction(prompt):
+    while True:
+        try:
+            frac = (input(prompt))
+            x, y = frac.split("/")
+            x, y = int(x), int(y)
+            if x <= y:
+                fuel = (int(x) / int(y)) * 100
+                if 99 <= fuel <= 100:
+                    return "F"
+                elif 0 <= fuel <= 1:
+                    return "E"
+                elif 1 < fuel < 99:
+                    return f"{round(fuel)}%"
+                else:
+                    pass
+        except ValueError or ZeroDivisionError:
+            pass
+
+
+if __name__ == "__main__":
+    main()
 
