43dc2fb221327bd628b34c8ddb6fe410f6656381
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-20 15:31:27 -0500

    Update nutrition_revision.py

--- week2/nutrition/nutrition_revision.py
+++ week2/nutrition/nutrition_revision.py
@@ -1,27 +1,28 @@
-fruits: dict[str, int] = {
-    "apple": 130,
-    "avocado": 50,
-    "banana": 110,
-    "cantaloupe": 50,
-    "grapefruit": 60,
-    "grapes": 90,
-    "honeydew melon": 50,
-    "kiwifruit": 90,
-    "lemon": 15,
-    "lime": 20,
-    "nectarine": 60,
-    "orange": 80,
-    "peach": 60,
-    "pear": 100,
-    "pineapple": 50,
-    "plums": 70,
-    "strawberries": 50,
-    "sweet cherries": 100,
-    "tangerine": 50,
-    "watermelon": 80,
+fruits: dict[str, int]= {
+    "apple":           130,
+    "banana":          110,
+    "pear":            100,
+    "sweet cherries":  100,
+    "grapes":           90,
+    "kiwifruit":        90,
+    "orange":           80,
+    "watermelon":       80,
+    "plums":            70,
+    "grapefruit":       60,
+    "nectarine":        60,
+    "peach":            60,
+    "avocado":          50,
+    "cantaloupe":       50,
+    "honeydew melon":   50,
+    "pineapple":        50,
+    "strawberries":     50,
+    "tangerine":        50,
+    "lime":             20,
+    "lemon":            15,
 }
 
 fruit = input("Item: ").lower()
 if fruit in fruits:
     print("Calories:", fruits[fruit])
 
+# // LOL nothing to really fix just thought I'd make it look nicer for fun
