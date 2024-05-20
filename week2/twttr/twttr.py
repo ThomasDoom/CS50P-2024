fc0c955ebeeb3c92b29cbf23feefe1e3eea7f5c5
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-20 11:26:36 -0500

    Update twttr.py

--- week2/twttr/twttr.py
+++ week2/twttr/twttr.py
@@ -1,1 +1,25 @@
+def main():
+    word = input("Input: ")
+    print("Output:", remove_vowels(word))
 
+
+def remove_vowels(s) -> str:
+    vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
+    result = ""
+
+    # Check each char in s
+    for c in s:
+
+        # Ignore vowels
+        if c in vowels:
+            continue
+
+        # Add else to 'result' string
+        else:
+            result += c
+
+    return result
+
+
+if __name__ == "__main__":
+    main()
