886571f88a9cd52ef579082544751f646596c34b
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-20 17:50:24 -0500

    Update fuel_revision.py

--- week3/fuel/fuel_revision.py
+++ week3/fuel/fuel_revision.py
@@ -1,26 +1,29 @@
 def main():
-    print(get_fraction("Fraction: "))
+    print(calculate_fuel_status("Fraction: "))
 
 
-def get_fraction(prompt):
+def calculate_fuel_status(prompt):
+    """
+    Calculate the fuel status based on the provided fuel fraction.
+    Returns:
+        - "F" for full,
+        - "E" for empty,
+        - Percentage for other cases.
+    """
     while True:
         try:
             # Gets input -> splits string into substrings and converts into integers
-            x, y = map(int, (input(prompt)).split("/"))
+            num, den = map(int, input(prompt).split("/"))
 
-            # Checks for valid fraction
-            if x <= y and y != 0:
-
-                # Converting from decimal to percent value
-                fuel: int = int(round((x / y) * 100))
-                if fuel <= 1:
-                    return "E"
-                if fuel >= 99:
-                    return "F"
+            # Checks for valid fraction and non-zero denominator
+            if num <= den and den != 0:
+                fuel_perc = round((num / den) * 100) # Decimal to percent value
+                if fuel_perc <= 1:
+                    return "E"  # Empty
+                elif fuel_perc >= 99:
+                    return "F"  # Full
                 else:
-                    return f"{fuel}%"
-            else:
-                pass
+                    return f"{fuel_per}%"  # Percentage
 
         except (ValueError, ZeroDivisionError):
             pass
