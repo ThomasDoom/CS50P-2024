3ee6995ebccf1426f7500fdd5a4adf7393ae8a0e
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-20 14:36:24 -0500

    Update plates_revision.py

--- week2/plates/plates_revision.py
+++ week2/plates/plates_revision.py
@@ -7,17 +7,17 @@ def main():
 
 
 def is_valid(s: str) -> bool:
-    return (2 <= len(s) <= 6
-            and s[:2].isalpha()
-            and s.isalnum()
+    return (2 <= len(s) <= 6 # Check length
+            and s[:2].isalpha() # Check first 2 char are letters
+            and s.isalnum() # Check for punctuation/spaces
             and (s.isalpha()
-                 or number_check(s)))
+                 or number_check(s))) # If there are numbers go through function
 
 
 def number_check(s: str) -> bool:
-    for i, c in enumerate(s):
+    for i, c in enumerate(s): # Enumerate = [(2, 'C'), (3, 'S'), (4, '5'), (5, '0')]
         if c.isdigit():
-            return c != "0" and s[i:].isdigit()
+            return c != "0" and s[i:].isdigit() # Once first digit is found, check if 0 and check if remainder isdigit
 
 
 main()
