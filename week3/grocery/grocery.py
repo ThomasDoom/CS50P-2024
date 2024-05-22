562469451f5153afe58d0ca206819aac5b61d8b7
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-22 14:29:47 -0500

    Create grocery.py

---
+++ week3/grocery/grocery.py
@@ -0,0 +1,33 @@
+
+item_count: dict[str, int] = {}
+
+
+def main():
+    """
+    Get input, count their occurrences, and print the counts alphabetically
+    """
+    try:
+        while True:
+            item = input().upper()
+            if item in item_count:
+                item_count[item] += 1
+            else:
+                item_count[item] = 1
+    except EOFError:
+        print_sorted_items(item_count)
+
+
+
+def print_sorted_items(counts: dict[str, int]):
+    """
+    Prints the item counts sorted alpha by item names
+    """
+    sorted_dict = {key: value for key, value in sorted(counts.items())}
+    for key, value in sorted_dict.items():
+        print(f'{value} {key}')
+
+
+if __name__ == "__main__":
+    main()
+
+# THIS TOOK FOREVER TO FIGURE OUTTTTTT
