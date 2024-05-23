95719908911afb3e26a58c914280a3bfbc013808
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-22 19:42:31 -0500

    Update outdated_revision.py

--- week3/outdated/outdated_revision.py
+++ week3/outdated/outdated_revision.py
@@ -33,4 +33,4 @@ if __name__ == "__main__":
     main()
 
 
-# i could rewrite this with helper functions inside of main() to make it nicer and more maintainable but this problem took all my patience and tears so this is all we're gettin for now
+# i could rewrite this with helper functions inside of main() to make it nicer and more maintainable but this problem took all my patience and tears so we're copin with one big main function for now
