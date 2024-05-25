2aed50d38ff69709ed00a5a9dc6c38546d978eed
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-25 16:36:14 -0500

    Update and rename adieu_with_import to adieu_with_import.py

---
+++ week4/adieu/adieu_revision/adieu_with_import.py
@@ -0,0 +1,11 @@
+import inflect
+
+p = inflect.engine()
+names = []
+
+while True:
+    try:
+        names.append(input("Name: ").strip())
+    except EOFError:
+        print("\nAdieu, adieu, to", p.join(names))
+        break
