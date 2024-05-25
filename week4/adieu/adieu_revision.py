6c0cfabc19e242003deab18c1b81b33dae651cd6
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-25 15:57:29 -0500

    Update adieu_revision.py

--- week4/adieu/adieu_revision.py
+++ week4/adieu/adieu_revision.py
@@ -1,1 +1,12 @@
+# ....... i didnt open the hint LOL
+import inflect
 
+p = inflect.engine()
+names = []
+
+while True:
+    try:
+        names.append(input("Name: ").strip())
+    except EOFError:
+        print("\nAdieu, adieu, to", p.join(names))
+        break
