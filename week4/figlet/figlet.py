ad7e2f272d7e85837f916ecd773c535c60766892
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-25 14:51:04 -0500

    Update figlet.py

--- week4/figlet/figlet.py
+++ week4/figlet/figlet.py
@@ -1,1 +1,25 @@
+import sys
+from pyfiglet import Figlet
+from random import choice
+
+f = Figlet()
+fonts = f.getFonts()
+
+
+try:
+    if sys.argv[1] not in ('-f', '--font'):
+        raise Exception
+
+    f.setFont(font=sys.argv[2])
+
+except IndexError:
+    f.setFont(font=choice(fonts))
+
+except (TypeError, Exception):
+    sys.exit("Invalid Usage")
+
+
+s = input("Input: ")
+
+print("Output:\n", f.renderText(s))
 
