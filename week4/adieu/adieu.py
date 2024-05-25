7d82c22bd2e5b1a132dcc8221c0abbad6f6ea5d3
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-25 15:48:37 -0500

    Update adieu.py

--- week4/adieu/adieu.py
+++ week4/adieu/adieu.py
@@ -1,1 +1,18 @@
+names = []
+while True:
+    try:
+        name = input("Name: ")
+        names.append(name)
 
+    except EOFError:
+        break
+
+print("\nAdieu, adieu, to", end=' ')
+if len(names) > 2:
+    for n in names[:-1]:
+        print(n, end=', ')
+    print("and", names[-1])
+elif len(names) == 2:
+        print(names[0], "and", names[1])
+else:
+     print(names[0])
