94b7e49a21559bc93699e98caf936576fb7c6cf2
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-25 19:57:08 -0500

    Update game_revision.py

--- week4/game/game_revision.py
+++ week4/game/game_revision.py
@@ -1,18 +1,37 @@
 from random import randint
 
 def main():
+    # Loop till user wins
     while True:
         try:
-            level = int(input("Level: "))
+            level = get_user_input()
+
+            # Generate answer
             answer = randint(1, level)
+
             guess_and_check(answer)
-        except ValueError:
+
+        # For non-integers
+        except (ValueError, Exception):
             pass
+
+
+def get_user_input() -> int:
+    level = int(input("Level: "))
+
+    # Check for positive integer
+    if level > 0:
+        return level
+
+    # Fail case for negative integer and 0
+    raise Exception
 
 
 def guess_and_check(answer: int) -> str:
     while True:
         guess = int(input("Guess: "))
+
+        # Determine loss or win (less, more, equal)
         if guess > answer:
             print("Too large!")
         elif guess < answer:
