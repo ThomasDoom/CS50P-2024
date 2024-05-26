f57b11eb35ddef184fc2b34b94bdfd6831183a89
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-25 20:17:49 -0500

    Update deep_revision.py

--- week1/deep/deep_revision.py
+++ week1/deep/deep_revision.py
@@ -1,6 +1,6 @@
 thought = input("What is the Answer to the Greatest Question of life, the Universe, and Everything? ").lower().strip()
 
-if thought == "42" or thought == "forty two" or thought == "forty-two":
+if thought in ("42", "forty two", "forty-two"):
     print("Yes")
 else:
     print("No")
