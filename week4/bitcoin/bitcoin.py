5a6a4f49f76f7f2683a0be907c12ff611fee2f21
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-05-27 18:02:21 -0500

    Update bitcoin.py

--- week4/bitcoin/bitcoin.py
+++ week4/bitcoin/bitcoin.py
@@ -1,1 +1,20 @@
+#  USER SPECIFIES A NUMBER AMOUNT OF BITCOIN [n] IN COMMAND LINE
+#  IF AMOUNT ISN'T FLOAT -> EXIT PROGRAM VIA sys.exit WITH AN ERROR MSG
+#  USE API TO GET AND PARSE JSON OBJECT FROM https://api.coindesk.com/v1/bpi/currentprice.json TO GET CURRENT PRICE OF BITCOIN
+#  BE SURE TO CATCH AN EXCEPTIONS
+#  OUTPUT CURRENT COST OF n BITCOIN TO 4 DECIMAL PLACES USING [,] AS A THOUSANDS SEPARATOR
+import json
+import requests
+import sys
+
+try:
+    query = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    rate = query.json()['bpi']['USD']['rate_float']
+    amount = rate * float(sys.argv[1])
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+except IndexError:
+    sys.exit("Missing command-line argument")
+else:
+    print(f"Your amount of {sys.argv[1]} BTC is ${amount:,.4f}")
 
