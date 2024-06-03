e1f14b7d3b1b7fb8c89a7b340841efe6db7d7a67
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-02 21:42:02 -0500

    Update test_twttr.py

--- week5/test_twttr/test_twttr.py
+++ week5/test_twttr/test_twttr.py
@@ -1,1 +1,15 @@
+from twttr import shorten
 
+def test_limits():
+    assert shorten("AEIOUaeiou") == ""
+    assert shorten("") == ""
+    assert shorten("!,.:a:.,?") == "!,.::.,?"
+
+def test_main():
+    assert shorten("Hello, World!") == "Hll, Wrld!"
+    assert shorten("just setting up my twttr") == "jst sttng p my twttr"
+    assert shorten("What's your name?") == "Wht's yr nm?"
+
+def test_numbers():
+    assert shorten("H3ll0, w0r1d") == "H3ll0, w0r1d"
+    assert shorten("1e23456789a") == "123456789"
