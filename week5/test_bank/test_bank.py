6601ebf709c71deb940967a142d1072780765c6e
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-02 22:12:32 -0500

    Update test_bank.py

--- week5/test_bank/test_bank.py
+++ week5/test_bank/test_bank.py
@@ -1,1 +1,16 @@
+from bank import value
 
+def test_hello():
+    assert value("HELLO!") == 0
+    assert value("hello!") == 0
+    assert value("Hello, there!") == 0
+
+def test_hey():
+    assert value("Hey") == 20
+    assert value("hey, boss.") == 20
+    assert value("howdy, partner!") == 20
+
+def test_bonjour():
+    assert value("Bonjour") == 100
+    assert value("What's up?") == 100
+    assert value("What do you want...") == 100
