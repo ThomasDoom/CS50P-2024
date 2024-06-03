2477a9b17b9207bf302ed13817d0e1cae0bc1b50
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-02 22:36:04 -0500

    Update test_plates.py

--- week5/test_plates/test_plates.py
+++ week5/test_plates/test_plates.py
@@ -13,16 +13,19 @@ def test_length():
     assert is_valid("HelloWorld") == False
     assert is_valid("A") == False
 
+
 def test_startswith():
     assert is_valid("232323") == False
     assert is_valid("H3110") == False
     assert is_valid("AA3333") == True
+
 
 def test_numbers():
     assert is_valid("HE11O") == False
     assert is_valid("HE110") == True
     assert is_valid("HAL04") == False
     assert is_valid("HA104") == True
+
 
 def test_punctuation():
     assert is_valid("Hello.") == False
