8528894817890b84d2d25695224773b2528f9544
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-02 23:35:53 -0500

    Update test_fuel.py

--- week5/test_fuel/test_fuel.py
+++ week5/test_fuel/test_fuel.py
@@ -1,1 +1,23 @@
+from fuel import convert, gauge
+import pytest
 
+def test_convert():
+    assert convert("0/4") == 0
+    assert convert("1/4") == 25
+    assert convert("3/4") == 75
+    assert convert("4/4") == 100
+
+def test_gauge():
+    assert gauge(1) == "E"
+    assert gauge(25) == "25%"
+    assert gauge(75) == "75%"
+    assert gauge(99) == "F"
+
+def test_errors():
+    with pytest.raises(ValueError):
+        convert("three/four")
+        convert("1.5/3")
+
+    with pytest.raises(ZeroDivisionError):
+        convert("4/0")
+        convert("0/0")
