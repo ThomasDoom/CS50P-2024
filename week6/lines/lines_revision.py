6d96823d85f70f48217c43fb84128a5130c270a8
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-06 15:56:31 -0500

    Update lines_revision.py

--- week6/lines/lines_revision.py
+++ week6/lines/lines_revision.py
@@ -1,9 +1,9 @@
-import sys
+from sys import argv, exit
 
 
 def main():
     if check_for_file():
-        lines_of_code()
+        print(lines_of_code())
 
 
 def check_for_file() -> bool:
@@ -12,14 +12,14 @@ def check_for_file() -> bool:
         # Index must be 1
         # argument must be a python file
     """
-    if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
+    if len(argv) < 2:
+        exit("Too few command-line arguments")
 
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
+    elif len(argv) > 2:
+        exit("Too many command-line arguments")
 
-    if not sys.argv[1].endswith(".py"):
-        sys.exit("Not a Python File")
+    if not argv[1].endswith(".py"):
+        exit("Not a Python File")
 
     return True
 
@@ -29,21 +29,22 @@ def lines_of_code() -> int:
     Opens existing file, reads line by line and only counts valid lines of code
     """
     try:
-        file = open(sys.argv[1]).read().splitlines()
+        file = open(argv[1]).read().splitlines()
         lines = 0
+
         for row in file:
-            
+
             row = row.strip()
+
             # EMPTY STRINGS/LISTS AND COMMENTS RETURN FALSE
             if row and not row.startswith("#"):
                 lines += 1
 
-        print(lines)
+        return lines
 
     except FileNotFoundError:
-        sys.exit("File does not exist")
+        exit("File does not exist")
 
 
 if __name__ == "__main__":
     main()
-
