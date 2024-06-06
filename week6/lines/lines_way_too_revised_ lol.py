1b4e9fa8ff4d90b1ad03f5c9de49163adf9f5c67
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-06 14:28:26 -0500

    Create lines_way_too_revised_ lol.py

---
+++ week6/lines/lines_way_too_revised_ lol.py
@@ -0,0 +1,16 @@
+import sys; sys.exit("Invalid argument") if len(sys.argv) != 2 or not sys.argv[1].endswith(".py") else None
+try:
+    print(sum(1 for line in open(sys.argv[1]) if line.strip() and not line.lstrip().startswith("#")))
+except FileNotFoundError:
+    sys.exit("File does not exist")
+
+# HAHAHA the magic you can do with comprehensions (This is very bad code just to be clear i just wanted to see how crazy it can get ^_^)
+
+
+# but this isnt even the best we can do
+# you can go even further if u want
+# behold...
+
+import sys; sys.exit("Invalid argument") if len(sys.argv) != 2 or not sys.argv[1].endswith(".py") else print(sum(1 for line in open(sys.argv[1]) if line.strip() and not line.lstrip().startswith("#"))) if open(sys.argv[1]) else sys.exit("File does not exist")
+
+# least confusing code right there. horrifying. it passes the check50 tho so its clearly optimal /s
