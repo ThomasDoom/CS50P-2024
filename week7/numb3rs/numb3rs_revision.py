ae5f2e91abd391383534d54f7d6638d29329aaa0
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-11 12:31:52 -0500

    Update numb3rs_revision.py
    edited comments

--- week7/numb3rs/numb3rs_revision.py
+++ week7/numb3rs/numb3rs_revision.py
@@ -6,14 +6,14 @@ def main():
 
 
 def validate(ip: str) -> bool:
-    """Initial check for [X.X.X.X] format, ONLY integers"""
+    """Find [X.X.X.X] match, ONLY integers"""
     pattern = r"^\d+\.\d+\.\d+\.\d+$"
 
     return valid_bytes(ip) if re.match(pattern, ip) else False
 
 
 def valid_bytes(ip: str) -> bool:
-    """Final check for each number to be between 0-255"""
+    """Validate each number is 0-255"""
     bytes = ip.split(".")
     return all(0 <= int(byte) <= 255 for byte in bytes)
 
