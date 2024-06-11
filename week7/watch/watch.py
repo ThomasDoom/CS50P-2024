246f0879887c326d9c35890a4587ccf7c34d17c7
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-11 13:41:19 -0500

    Submit watch solution

--- week7/watch/watch.py
+++ week7/watch/watch.py
@@ -8,7 +8,7 @@ def main():
 def parse(s: str) -> str:
     """Search through iframe to find youtube link, group and return the embed code with the shortened URL"""
     try:
-        match = re.search(r'<iframe?.*youtube\.com/embed+/(\w+)', s, re.IGNORECASE)
+        match = re.search(r'<iframe.*youtube\.com/embed/(\w+)', s, re.IGNORECASE)
         embed = match.group(1)
         return f"https://youtu.be/{embed}"
 
