b1d606af116ab10bada0f77e1fe941dea093b07e
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-11 13:54:23 -0500

    Removed useless note

--- week7/watch/watch.py
+++ week7/watch/watch.py
@@ -8,10 +8,8 @@ def main():
 
 
 def parse(s: str) -> str:
-    """Search through iframe to find youtube link, group and return the embed code with the shortened URL"""
     if match := re.search(PATTERN, s):
         return f"https://youtu.be/{match.group(1)}"
-    return None
 
 
 if __name__ == "__main__":
