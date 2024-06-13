f00427d9bb87bd670aa82e5e4c712d4acd4a213f
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-06-13 12:20:54 -0500

    fix: added notes to working_revision.py

--- week7/working/working_revision.py
+++ week7/working/working_revision.py
@@ -9,15 +9,15 @@ def main():
     print(convert(input("Hours: ")))
 
 
-def convert(s: str):
+def convert(s: str) -> str:
     if match := re.match(r"^" + PATTERN + " to " + PATTERN + "$", s):
-        start_time = convert_24h(int(match.group(1)),
-                                 int(match.group(2)) if match.group(2) else 0,
-                                 match.group(3)
+        start_time = convert_24h(int(match.group(1)),  # START HOUR
+                                 int(match.group(2)) if match.group(2) else 0,  # START MINUTE, IF NONE SET TO 0
+                                 match.group(3)  # START PERIOD, AM|PM
                                  )
-        end_time = convert_24h(int(match.group(4)),
-                               int(match.group(5)) if match.group(5) else 0,
-                               match.group(6)
+        end_time = convert_24h(int(match.group(4)),  # END HOUR
+                               int(match.group(5)) if match.group(5) else 0,  # END MINUTE, IF NONE SET TO 0
+                               match.group(6)  # END PERIOD, AM|PM
                                )
         return f"{start_time} to {end_time}"
     else:
@@ -25,11 +25,11 @@ def convert(s: str):
 
 
 def convert_24h(hour: int, minute: int, period: str) -> str:
-    if period == "AM" and hour == 12:
+    if period == "AM" and hour == 12:  # 12AM SHOULD BE 0:00
         hour = 0
-    elif period == "PM" and hour != 12:
+    elif period == "PM" and hour != 12:  # ADD 12 TO HOUR IF ITS 1PM-11PM
         hour += 12
-    return f"{hour:02}:{minute:02}"
+    return f"{hour:02}:{minute:02}"  # RETURN IN 00:00 FORMAT
 
 
 if __name__ == "__main__":
