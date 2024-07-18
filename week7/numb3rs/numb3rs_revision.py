5f0b359182fb678f9eab98ce61b3a463132b75b9
Mac Doom <156052160+ThomasDoom@users.noreply.github.com>
2024-07-17 19:26:33 -0500

    fix: rearranged numb3rs_revision.py

--- week7/numb3rs/numb3rs_revision.py
+++ week7/numb3rs/numb3rs_revision.py
@@ -1,13 +1,14 @@
 import re
 
+PATTERN = r"^\d+\.\d+\.\d+\.\d+$"
+
 def main():
     print(validate(input("IPv4 Address: ")))
 
 
 def validate(ip: str) -> bool:
-    """Find [X.X.X.X] match then return if each number is 0-255"""
-    pattern = r"^\d+\.\d+\.\d+\.\d+$"
-    if not re.match(pattern, ip):
+    """Find [X.X.X.X] match then return if each number is between 0-255"""
+    if not re.match(PATTERN, ip):
         return False
     return all(0 <= int(byte) <= 255 for byte in ip.split("."))
 
